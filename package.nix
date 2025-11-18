696e35a73dba8d4c0c1a229c5884ba327b83c57a
éclairevoyant <848000+eclairevoyant@users.noreply.github.com>
2025-11-17 20:22:49 -0500

    fix(nix): use reproducible source path

--- package.nix
+++ package.nix
@@ -1,11 +1,21 @@
 { lib, rustPlatform }:
 let
-  toml = (lib.importTOML (./Cargo.toml)).package;
+  toml = (lib.importTOML ./Cargo.toml).package;
+  fs = lib.fileset;
+  s = ./.;
 in
 rustPlatform.buildRustPackage {
   pname = toml.name;
   inherit (toml) version;
-  src = ./.;
+  src = fs.toSource {
+    root = s;
+    fileset = fs.unions [
+      (s + /src)
+      (s + /tests)
+      (s + /Cargo.lock)
+      (s + /Cargo.toml)
+    ];
+  };
   cargoLock.lockFile = ./Cargo.lock;
 
   meta.mainProgram = "smfh";
